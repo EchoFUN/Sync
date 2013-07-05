a1b6397a2481dc3f0393b341cb1928a415564001
EchoFUN <xukai.ken@gmail.com>
2013-07-05 18:25:26 +0800

    Finish the series interface.

--- app.js
+++ app.js
@@ -26,39 +26,42 @@
 
    var Sync = {};
 
-   Sync.parallel = function(tasks, callback) {
+   Sync.series = function(tasks, callback) {
+      
+      // 判断不同的参数结构，执行不同的方法体。
       if (tasks.constructor === Array) {
-
          if (tasks.length) {
             var tasksLength = tasks.length;
             var firstTask = tasks.shift();
 
-            var errorStack = [], resultStack = [];
+            var resultStack = [], taskCounter = 0, error = null;
+            var signal = function(taskError, result) {
 
-            var ProcessFeedBack = function(errors, results) {
-
-            }
-            var signal = function(error, result) {
+               // 任务执行计数器加一。
+               taskCounter++;
+               
+               // 把执行结果和错误压入堆栈。
+               resultStack.push(result);
+               error = taskError || error;
                if (tasks.length) {
                   var nextTask = tasks.shift();
                   nextTask(signal);
                } else {
-                  callback(errorStack, resultStack);
+                  callback(error, resultStack);
                }
             };
             firstTask(signal);
          }
-
       } else {
-
+         ;
       }
    };
 
-   Sync.series = function(tasks, callback) {
+   Sync.parallel = function(tasks, callback) {
       if (tasks.constructor === Array) {
-
+         
       } else {
-
+         ;
       }
    };
 
@@ -66,20 +69,37 @@
 })();
 
 // 实测代码区域
-Sync.parallel([
+/* Sync.series([
    function(callback) {
-      callback({}, 0);
+      callback(null, 0);
    },
    function(callback) {
-      callback({}, 1);
+      callback(null, 1);
    },
    function(callback) {
-      callback({}, 2);
+      callback(null, 2);
    }],
-
+   
    // 最终触发的回调函数
    function(error, results) {
-      console.log(error);
+      error && console.log(error.message);
       console.log(results);
    }
-);
+); */
+
+Sync.parallel([
+      function(callback) {
+         
+      },
+      function(callback) {
+         
+      },
+      function(callback) {
+         
+      }
+   ],
+   
+   function(error, results) {
+      
+   }
+)
